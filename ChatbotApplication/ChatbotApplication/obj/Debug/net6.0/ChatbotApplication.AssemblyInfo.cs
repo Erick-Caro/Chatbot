600c4f2dfc5c876439dfc2328b8eca8abfe64a08
Erick-Caro <erick.caro.h@gmail.com>
2026-04-01 14:16:15 -0300

    actualizacion

--- ChatbotApplication/ChatbotApplication/obj/Debug/net6.0/ChatbotApplication.AssemblyInfo.cs
+++ ChatbotApplication/ChatbotApplication/obj/Debug/net6.0/ChatbotApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatbotApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+285f26e91a4707ee4d5e8842b81e359d5f1c36ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf9217d3122ab136664f31e60fa2ac5f9c5f4513")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatbotApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatbotApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
